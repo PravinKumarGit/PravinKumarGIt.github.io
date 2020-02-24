0357f437bddb42d3e4fbe7ef289c719605813adf
Michael Crook <michaelc@fairgofinance.com.au>
2020-02-24 12:13:48 +0800

    typos and non html attribute cleanup

--- src/models/loanForm.js
+++ src/models/loanForm.js
@@ -6,7 +6,6 @@ export default class LoanForm {
   }
 
   inIt({ values, step }) {
-    console.log(values,step,'model')
     // step 1
     this.source = "App Screen 1 - Basic info";
     this.loanAmount = values.loanAmount || "";
