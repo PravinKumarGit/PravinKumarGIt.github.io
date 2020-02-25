3e949bf4b81d1df117bedc8b42c7b9266897f8f8
jasontien-fgf <jasont@fairgofinance.com.au>
2020-02-25 13:43:52 +0800

    2315 - PR review changes

--- src/constants/environmentSettings.js
+++ src/constants/environmentSettings.js
@@ -1,6 +1,6 @@
 const environmentSettings = {
-  getPricingAPi: process.env.REACT_APP_GET_PRICING_API,
-  submitApplicationApi: process.env.REACT_APP_SUBMIT_APPLICATION_API,
+  pricingBaseUrlApi: process.env.REACT_APP_PRICING_BASE_URL_API,
+  applicationProcessingBaseUrlApi: process.env.REACT_APP_APPLICATION_PROCESSING_BASE_URL_API,
   googleApiKey: process.env.REACT_APP_GOOGLE_API_KEY
 }
 
