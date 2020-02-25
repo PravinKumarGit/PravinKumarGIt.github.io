ccdc0bafa4e39d7183921a021131f13ab30d3a89
jasontien-fgf <jasont@fairgofinance.com.au>
2020-02-25 10:01:05 +0800

    2315 - Fix typo on env file

--- src/constants/environmentSettings.js
+++ src/constants/environmentSettings.js
@@ -1,6 +1,6 @@
 const environmentSettings = {
   getPricingAPi: process.env.REACT_APP_GET_PRICING_API,
-  submitApplicationApi: process.env.REACT_APP_SUMBIT_APPLICATION_API,
+  submitApplicationApi: process.env.REACT_APP_SUBMIT_APPLICATION_API,
   googleApiKey: process.env.REACT_APP_GOOGLE_API_KEY
 }
 
