8575c99b17c46f716a555295c78f4d9fb26bb164
jasontien-fgf <jasont@fairgofinance.com.au>
2020-02-25 14:10:52 +0800

    2315 - Fix invalid string template

--- src/services/apiRoutes.js
+++ src/services/apiRoutes.js
@@ -2,6 +2,6 @@ import environmentSettings from "../constants/environmentSettings";
 
 export default {
   BASE_URL: "",
-  LOAN_AMOUNT: `"${environmentSettings.pricingBaseUrlApi}/price/prices`,
+  LOAN_AMOUNT: `${environmentSettings.pricingBaseUrlApi}/price/prices`,
   POST_LOAN_FORM: environmentSettings.applicationProcessingBaseUrlApi
 };
